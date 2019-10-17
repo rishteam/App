dca39a78fe698f3733d2a793bc50dd736add6f89
ICEJJ <lpc0503@gmail.com>
2019-10-17 13:53:29 +0800

    app

---
+++ FJU_Course_Registration_Sys_RIsh/app/src/main/java/com/example/fju_course_registration_sys_rish/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fju_course_registration_sys_rish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
