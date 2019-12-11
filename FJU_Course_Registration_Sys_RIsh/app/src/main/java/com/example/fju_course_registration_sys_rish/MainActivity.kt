7913d1b04205270ee64aa212c3b263f563047ee6
SunTalk <infernal56727@gmail.com>
2019-12-11 16:01:20 +0800

    [login] can get users token now

--- FJU_Course_Registration_Sys_RIsh/app/src/main/java/com/example/fju_course_registration_sys_rish/MainActivity.kt
+++ FJU_Course_Registration_Sys_RIsh/app/src/main/java/com/example/fju_course_registration_sys_rish/MainActivity.kt
@@ -6,8 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header_home.*
+import kotlinx.android.synthetic.main.nav_header_home.view.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,7 +29,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when(v?.id){
 
             R.id.Login -> toHomePage()
-            R.id.Registration -> toRegisterPage()
+//            R.id.Registration -> toRegisterPage()
 
         }
     }
@@ -27,22 +39,53 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         Login.setOnClickListener(this)
-        Registration.setOnClickListener(this)
+//        Registration.setOnClickListener(this)
 
     }
 
     private fun toHomePage(){
 
         Log.i("Tag", "toHomePage")
-        Toast.makeText(this@MainActivity, "Login Success", Toast.LENGTH_SHORT).show()
-        startActivity(Intent(this, Home::class.java))
-        finish()
+        val account = Account.text.toString()
+        val password = PWD.text.toString()
+
+        val body: MutableMap<String,String> = HashMap<String, String>()
+        body["username"] = account
+        body["password"] = password
+        val json = JSONObject(body as Map<*, *>)
+
+        Log.i("Login",body.toString())
+        val url = "http://vm.rish.com.tw/db/v1/login"
+        val que = Volley.newRequestQueue(this)
+        val req = object : JsonObjectRequest(Request.Method.POST, url, json,
+            Response.Listener {
+                    response ->
+                Log.i("Login","Success")
+                Log.i("Login",response.toString())
+
+                val token = response.getString("token")
+                Log.i("token",token)
+
+                Toast.makeText(this@MainActivity, "Login Success", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, Home::class.java))
+                finish()
+
+            }, Response.ErrorListener {
+                    error ->
+                Toast.makeText(this@MainActivity, "Login Failed", Toast.LENGTH_SHORT).show()
+                Log.i("Login","Fuck")
+            }){}
+
+        que.add(req)
+
     }
 
-    private fun toRegisterPage(){
 
-        Log.i("Tag", "toRegisterPage")
-        Toast.makeText(this@MainActivity, "Register Success", Toast.LENGTH_SHORT).show()
+//    private fun toRegisterPage(){
+//
+//        Log.i("Tag", "toRegisterPage")
+//        Toast.makeText(this@MainActivity, "Register Success", Toast.LENGTH_SHORT).show()
+//
+//    }
 
-    }
 }
