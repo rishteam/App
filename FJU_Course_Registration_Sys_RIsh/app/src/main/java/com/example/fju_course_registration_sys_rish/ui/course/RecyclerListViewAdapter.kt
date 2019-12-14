63b1f45a15678dc00f2ace3fccd1ad11729f5e01
ICEJJ <lpc0503@gmail.com>
2019-12-14 15:44:55 +0800

    listView finish

--- FJU_Course_Registration_Sys_RIsh/app/src/main/java/com/example/fju_course_registration_sys_rish/ui/course/RecyclerListViewAdapter.kt
+++ FJU_Course_Registration_Sys_RIsh/app/src/main/java/com/example/fju_course_registration_sys_rish/ui/course/RecyclerListViewAdapter.kt
@@ -12,6 +12,7 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fju_course_registration_sys_rish.R
 import com.example.fju_course_registration_sys_rish.course_detail
+import org.w3c.dom.Text
 
 class RecyclerListViewAdapter(private val course : MutableList<Course>, private val context: Context) : RecyclerView.Adapter<RecyclerListViewAdapter.ViewHolder>(){
 
@@ -24,14 +25,18 @@ class RecyclerListViewAdapter(private val course : MutableList<Course>, private 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         Log.i("adapter", "onBind")
-        holder.textViewID.text = course[position].id.toString()
-        holder.textViewUname.text = course[position].schoolName
+        holder.textViewDepartment.text = course[position].department
+        holder.textViewKind.text = course[position].kind
+        holder.textViewCourseName.text = course[position].courseName
+        holder.textViewDay.text = course[position].day
+        holder.textViewPeriod.text = course[position].period
+        holder.textViewClassroom.text = course[position].classroom
         holder.textViewProfessor.text = course[position].professor
 
 
         holder.itemView.setOnClickListener{
 
-            Toast.makeText(it.context, "ID: " + course[position].id.toString() + " school: " + course[position].schoolName + " is clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(it.context, course[position].courseName + " is chosen", Toast.LENGTH_SHORT).show()
             val intent = Intent(it.context, course_detail::class.java)
             intent.putExtra("Data", course[position])
             it.context.startActivity(intent)
@@ -46,8 +51,16 @@ class RecyclerListViewAdapter(private val course : MutableList<Course>, private 
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
 
-        val textViewID : TextView = view.findViewById(R.id.id)
-        val textViewUname : TextView = view.findViewById(R.id.Uname)
+        val textViewKind : TextView = view.findViewById(R.id.kind)
+        val textViewCourseName : TextView = view.findViewById(R.id.CourseName)
         val textViewProfessor: TextView = view.findViewById(R.id.ProfessorName)
+        val textViewDepartment : TextView = view.findViewById(R.id.department)
+        val textViewDay : TextView = view.findViewById(R.id.day)
+        val textViewPeriod : TextView = view.findViewById(R.id.period)
+        val textViewClassroom : TextView = view.findViewById(R.id.classroom)
+
+        //        val textViewID : TextView = view.findViewById(R.id.id)
+//        val textViewUname : TextView = view.findViewById(R.id.Uname)
+
     }
 }
